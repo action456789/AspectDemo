31947a7068cdd73cc09ba0e4304a918b0e54bd46
KeSen <317331820@qq.com>
2015-11-22 11:23:19 +0800

    init

--- AspectsDemo/ViewController.m
+++ AspectsDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Aspects.h"
 
 @interface ViewController ()
 
@@ -16,12 +17,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeContactAdd];
+    [btn addTarget:self action:@selector(buttonClieckEvent:) forControlEvents:UIControlEventTouchUpInside];
+    btn.center = self.view.center;
+    [self.view addSubview:btn];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)test {
+    NSLog(@"test is invoked");
+}
+
+- (void)buttonClieckEvent:(UIButton *)sender{
+    
+    UIViewController *testController = [[UIImagePickerController alloc] init];
+    testController.modalPresentationStyle = UIModalPresentationFormSheet;
+    [self presentViewController:testController animated:YES completion:NULL];
+    
+    /** We are interested in being notified when the controller is being dismissed.
+    *  block 中的第一个参数是添加钩子的对象本身，其他参数被添加钩子函数的参数（也可省略）
+    **/
+    [testController aspect_hookSelector:@selector(viewWillDisappear:)
+                            withOptions:AspectPositionAfter
+                             usingBlock:^(id<AspectInfo> info, BOOL animated) {
+        
+                                 UIViewController *controller = [info instance];
+        
+                                 if (controller.isBeingDismissed || controller.isMovingFromParentViewController) {
+
+                                     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"消息" message:@"viewWillDisappear is called!" preferredStyle:UIAlertControllerStyleAlert];
+                                
+                                     [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDestructive handler:nil]];
+                                
+                                     [self presentViewController:alert animated:YES completion:nil];
+         }
+    }                             error:NULL];
+    
+    // Hooking dealloc is delicate, only AspectPositionBefore will work here.
+    [testController aspect_hookSelector:NSSelectorFromString(@"dealloc")
+                            withOptions:AspectPositionBefore
+                             usingBlock:^(id<AspectInfo> info) {
+                                 
+                                 NSLog(@"Controller is about to be deallocated: %@", [info instance]);
+    }
+                                  error:NULL];
 }
 
 @end
